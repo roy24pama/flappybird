d8fb7a0f48b1adb435ccc455350289e0f4a45275
roy24pama <roympama@gmail.com>
2017-05-26 22:23:06 +0800

    Add files via upload

---
+++ FlappyDemo1/android/src/com/cboiser/game/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.cboiser.game;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.cboiser.game.FlappyDemo1;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new FlappyDemo1(), config);
+	}
+}
